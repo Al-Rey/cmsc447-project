f48b297787a17eb746bf81ebb9b8c195c188d010
James B <jbartko1@umbc.edu>
2022-10-16 09:36:43 -0400

    Updatev3
    Added buttons for sorting by generation also Search bar

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -1,22 +1,90 @@
 const pokedex = document.getElementById('pokedex');
-
-const fetchPokemon = () => {
-    const promises = [];
-    for (let i = 1; i <= 251; i++) {
-        const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-        promises.push(fetch(url).then((res) => res.json()));
+const pokemons = [];
+const generation = [];
+function  fetchGeneration(generation){
+    const pokemon = [];
+    for (let i = 0; i < generation.length; i++) {
+        const url = `https://pokeapi.co/api/v2/generation/${generation[i]}`;
+        $.ajax({
+            url: url,
+            dataType: "json",
+            type: "Get",
+            async: true,
+            success: function (results) {
+                console.log(results);
+                for (let j = 0; j < results.pokemon_species.length; j++) {
+                    if (typeof results.pokemon_species[j] !== 'undefined'){
+                        var pathname = new URL(results.pokemon_species[j].url).pathname;
+                        const pokemon_id = pathname.split('/');
+                        //pokemons.push(pokemon_id[4]);
+                        const url = `https://pokeapi.co/api/v2/pokemon/${pokemon_id[4]}`;
+                        $.ajax({
+                            url: url,
+                            dataType: "json",
+                            type: "Get",
+                            async: true,
+                            success: function (results) {
+                                console.log(results);
+                                const pokemon2 = {
+                                    name: results.name,
+                                    image: results.sprites['front_default'],
+                                    type: results.types.map((type) => type.type.name).join(', '),
+                                    id: results.id
+                                    
+                                };
+                                t = `
+                                            <li class="card generation_${generation[i]}">
+                                                <img class="card-image" src="${pokemon2.image}"/>
+                                                <h2 class="card-title">${pokemon2.id}. ${pokemon2.name}</h2>
+                                                <p class="card-subtitle">Type: ${pokemon2.type}</p>
+                                                
+                                            </li>
+                                        `;
+                                $(pokedex).append(t);
+                                
+                                //pokemon.push(pokemon2);
+                                //displayPokemon(pokemon);
+                            },
+                            error: function (xhr, exception) {
+                                
+                            }
+                        });
+                        
+                    }
+                    
+                }
+                console.log(pokemon);
+            },
+            error: function (xhr, exception) {
+                
+            }
+        });
     }
-    Promise.all(promises).then((results) => {
-        const pokemon = results.map((result) => ({
-            name: result.name,
-            image: result.sprites['front_default'],
-            type: result.types.map((type) => type.type.name).join(', '),
-            id: result.id
-            
-        }));
-        displayPokemon(pokemon);
+    console.log(pokemons);
+}
+
+$('.generation_check_box').change(function(){
+        check = generation.includes($(this).val());
+        if(check == true){
+            const index = generation.indexOf($(this).val());
+            if (index > -1) { 
+              generation.splice(index, 1);
+            }
+            $('.generation_'+$(this).val()).remove();
+        }else{
+            generation.push($(this).val());
+            var new_generation = [$(this).val()];
+            fetchGeneration(new_generation);
+        }
+});
+
+$(".search_field").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $(".card").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
-};
+  });
+  
 
 const displayPokemon = (pokemon) => {
     console.log(pokemon);
@@ -34,6 +102,3 @@ const displayPokemon = (pokemon) => {
         .join('');
     pokedex.innerHTML = pokemonHTMLString;
 };
-
-
-fetchPokemon();
