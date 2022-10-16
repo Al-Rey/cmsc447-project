4f8b44065ed86879bfb2b73b8db9aed31e57a71c
James B <jbartko1@umbc.edu>
2022-10-16 09:41:23 -0400

    Updatev3

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -1,10 +1,8 @@
 const pokedex = document.getElementById('pokedex');
 const pokemons = [];
 const generation = [];
-var all_pokemon = [];
 function  fetchGeneration(generation){
-    all_pokemon = [];
-    $(pokedex).html('');
+    const pokemon = [];
     for (let i = 0; i < generation.length; i++) {
         const url = `https://pokeapi.co/api/v2/generation/${generation[i]}`;
         $.ajax({
@@ -13,7 +11,7 @@ function  fetchGeneration(generation){
             type: "Get",
             async: true,
             success: function (results) {
-                //console.log(results);
+                console.log(results);
                 for (let j = 0; j < results.pokemon_species.length; j++) {
                     if (typeof results.pokemon_species[j] !== 'undefined'){
                         var pathname = new URL(results.pokemon_species[j].url).pathname;
@@ -26,7 +24,7 @@ function  fetchGeneration(generation){
                             type: "Get",
                             async: true,
                             success: function (results) {
-                                //console.log(results);
+                                console.log(results);
                                 const pokemon2 = {
                                     name: results.name,
                                     image: results.sprites['front_default'],
@@ -35,14 +33,17 @@ function  fetchGeneration(generation){
                                     
                                 };
                                 t = `
-                                            <li class="card generation_${generation[i]}" id="${pokemon2.id}">
+                                            <li class="card generation_${generation[i]}">
                                                 <img class="card-image" src="${pokemon2.image}"/>
                                                 <h2 class="card-title">${pokemon2.id}. ${pokemon2.name}</h2>
                                                 <p class="card-subtitle">Type: ${pokemon2.type}</p>
                                                 
                                             </li>
                                         `;
-                                all_pokemon[pokemon2.id] = t;
+                                $(pokedex).append(t);
+                                
+                                //pokemon.push(pokemon2);
+                                //displayPokemon(pokemon);
                             },
                             error: function (xhr, exception) {
                                 
@@ -52,14 +53,14 @@ function  fetchGeneration(generation){
                     }
                     
                 }
-                //console.log(pokemon);
+                console.log(pokemon);
             },
             error: function (xhr, exception) {
                 
             }
         });
     }
-    
+    console.log(pokemons);
 }
 
 $('.generation_check_box').change(function(){
@@ -70,10 +71,10 @@ $('.generation_check_box').change(function(){
               generation.splice(index, 1);
             }
             $('.generation_'+$(this).val()).remove();
-            fetchGeneration(generation);
         }else{
             generation.push($(this).val());
-            fetchGeneration(generation);
+            var new_generation = [$(this).val()];
+            fetchGeneration(new_generation);
         }
 });
 
@@ -84,16 +85,20 @@ $(".search_field").on("keyup", function() {
     });
   });
   
- function displayPokemon(){
-     $(pokedex).html('');
-     all_pokemon.forEach((item, index)=>{
-         $(pokedex).append(item);
-});
 
- }
- 
-setInterval(function(){ 
-    displayPokemon();  
-}, 1000);
-  
-
+const displayPokemon = (pokemon) => {
+    console.log(pokemon);
+    const pokemonHTMLString = pokemon
+        .map(
+            (pokeman) => `
+        <li class="card">
+            <img class="card-image" src="${pokeman.image}"/>
+            <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+            <p class="card-subtitle">Type: ${pokeman.type}</p>
+            
+        </li>
+    `
+        )
+        .join('');
+    pokedex.innerHTML = pokemonHTMLString;
+};
