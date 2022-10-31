593be9ae2ae3ba44f570e5d80e6bddb70fd9b8b0
James B <jbartko1@umbc.edu>
2022-10-31 13:29:50 -0400

    Updatev5
    Fixed search bar, added a pupup window feature when user clicks on pokemon image

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -2,6 +2,7 @@ const pokedex = document.getElementById('pokedex');
 const pokemons = [];
 const generation = [];
 var all_pokemon = [];
+var display_check = 0;
 function  fetchGeneration(generation){
     all_pokemon = [];
     $(pokedex).html('');
@@ -14,6 +15,7 @@ function  fetchGeneration(generation){
             async: true,
             success: function (results) {
                 //console.log(results);
+                display_check = 0;
                 for (let j = 0; j < results.pokemon_species.length; j++) {
                     if (typeof results.pokemon_species[j] !== 'undefined'){
                         var pathname = new URL(results.pokemon_species[j].url).pathname;
@@ -29,8 +31,11 @@ function  fetchGeneration(generation){
                                 //console.log(results);
                                 const pokemon2 = {
                                     name: results.name,
+                                    weight: results.weight,
+                                    height: results.height,
                                     image: results.sprites['front_default'],
                                     type: results.types.map((type) => type.type.name).join(', '),
+                                    moves: results.moves.map((move) => move.move.name).join(', '),
                                     id: results.id
                                     
                                 };
@@ -39,6 +44,9 @@ function  fetchGeneration(generation){
                                                 <img class="card-image" src="${pokemon2.image}"/>
                                                 <h2 class="card-title">${pokemon2.id}. ${pokemon2.name}</h2>
                                                 <p class="card-subtitle">Type: ${pokemon2.type}</p>
+                                                <p class="card-subtitle card-moves">Move: ${pokemon2.moves}</p>
+                                                <p class="card-subtitle card-weight">Weight: ${pokemon2.weight}</p>
+                                                <p class="card-subtitle card-height">Height: ${pokemon2.height}</p>
                                                 
                                             </li>
                                         `;
@@ -62,6 +70,8 @@ function  fetchGeneration(generation){
     
 }
 
+var myInterval;
+
 $('.generation_check_box').change(function(){
         check = generation.includes($(this).val());
         if(check == true){
@@ -75,25 +85,57 @@ $('.generation_check_box').change(function(){
             generation.push($(this).val());
             fetchGeneration(generation);
         }
+        $(".search_field").val('');
+        myInterval = setInterval(displayPokemon, 1000);
 });
 
 $(".search_field").on("keyup", function() {
+    clearInterval(myInterval); // stop the interval
+    display_check = 1;
     var value = $(this).val().toLowerCase();
     $(".card").filter(function() {
       $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
     });
   });
   
- function displayPokemon(){
-     $(pokedex).html('');
-     all_pokemon.forEach((item, index)=>{
-         $(pokedex).append(item);
-});
 
+ function displayPokemon(){
+     console.log(1);
+     if(display_check == 0){
+        $(pokedex).html('');
+        all_pokemon.forEach((item, index)=>{
+             $(pokedex).append(item);
+        });
+        display_check = 1;
+     }
+    console.log(myInterval);
+    clearInterval(myInterval); // stop the interval
  }
  
-setInterval(function(){ 
-    displayPokemon();  
-}, 1000);
+ 
+ $(document).on('click', '.card', function(){
+    $('.modal_content').html($(this).html());
+    toggleModal();
+ });
+ 
+ 
+ const modal = document.querySelector(".modal");
+//const trigger = document.querySelector(".trigger");
+const closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+//trigger.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
   
 
