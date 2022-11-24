161058bb327a4dc93ba2cd04e29a33adf129672e
James B <jbartko1@umbc.edu>
2022-11-23 22:29:04 -0500

    Updatev6
    Added changes to enlarge popup window, add move list in an organize manner and other stats.

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -29,13 +29,25 @@ function  fetchGeneration(generation){
                             async: true,
                             success: function (results) {
                                 //console.log(results);
+                            types = results.types.map((type) => type.type.name).join(',');
+                            types = types.split(',').sort();
+                            moves = results.moves.map((move) => move.move.name).join(',');
+                            moves = moves.split(',').sort();
+                            stats = results.stats.map((stat) => stat.stat.name).join(',');
+                            stats = stats.split(',').sort();
+                            //abilities = results.abilities.map((abilitie) => abilitie.name).join(',');
+                            //abilities = abilities.split(',').sort();
+
+
                                 const pokemon2 = {
                                     name: results.name,
                                     weight: results.weight,
                                     height: results.height,
                                     image: results.sprites['front_default'],
-                                    type: results.types.map((type) => type.type.name).join(', '),
-                                    moves: results.moves.map((move) => move.move.name).join(', '),
+                                    type: types.map((type) => type).join(', '),
+                                    moves: moves.map((move) => move).join('<br>'),
+                                    stats: stats.map((stat) => stat).join('<br>'),
+                                    //abilities: abilities.map((abilitie) => abilitie).join('<br>'),
                                     id: results.id
                                     
                                 };
@@ -44,10 +56,12 @@ function  fetchGeneration(generation){
                                                 <img class="card-image" src="${pokemon2.image}"/>
                                                 <h2 class="card-title">${pokemon2.id}. ${pokemon2.name}</h2>
                                                 <p class="card-subtitle">Type: ${pokemon2.type}</p>
-                                                <p class="card-subtitle card-moves">Move: ${pokemon2.moves}</p>
                                                 <p class="card-subtitle card-weight">Weight: ${pokemon2.weight}</p>
                                                 <p class="card-subtitle card-height">Height: ${pokemon2.height}</p>
-                                                
+                                                <div class="d-flex">
+                                                    <div class="card-subtitle card-moves w-50"><h3>Stats</h3> <p class="md-section">${pokemon2.stats}</p></div>
+                                                    <div class="card-subtitle card-moves w-50"><h3>Move</h3> <p class="md-section">${pokemon2.moves}</p></div>
+                                                </div>
                                             </li>
                                         `;
                                 all_pokemon[pokemon2.id] = t;
@@ -72,7 +86,12 @@ function  fetchGeneration(generation){
 
 var myInterval;
 
+$(document).ready(function(){
+    $('.loading').hide(); 
+});
+
 $('.generation_check_box').change(function(){
+        $('.loading').show();
         check = generation.includes($(this).val());
         if(check == true){
             const index = generation.indexOf($(this).val());
@@ -106,6 +125,7 @@ $(".search_field").on("keyup", function() {
         all_pokemon.forEach((item, index)=>{
              $(pokedex).append(item);
         });
+        $('.loading').hide();
         display_check = 1;
      }
     console.log(myInterval);
