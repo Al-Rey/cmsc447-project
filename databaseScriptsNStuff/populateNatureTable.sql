2cdd9810bcf5cbab283cf36e7b80eac885521603
Evan France <efrantz3@umbc.edu>
2022-12-11 18:33:28 -0500

    re-updated the populate tables scripts to better fit the SQL standards

--- databaseScriptsNStuff/populateNatureTable.sql
+++ databaseScriptsNStuff/populateNatureTable.sql
@@ -4,28 +4,29 @@ CREATE TABLE IF NOT EXISTS natures(
     decreased_stat VARCHAR(15),
     increased_stat VARCHAR(15)
 );
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (0,'hardy',NULL,NULL);
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (1,'bold','attack','defense');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (2,'modest','attack','special-attack');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (3,'calm','attack','special-defense');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (4,'timid','attack','speed');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (5,'lonely','defense','attack');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (6,'docile',NULL,NULL);
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (7,'mild','defense','special-attack');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (8,'gentle','defense','special-defense');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (9,'hasty','defense','speed');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (10,'adamant','special-attack','attack');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (11,'impish','special-attack','defense');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (12,'bashful',NULL,NULL);
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (13,'careful','special-attack','special-defense');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (14,'rash','special-defense','special-attack');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (15,'jolly','special-attack','speed');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (16,'naughty','special-defense','attack');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (17,'lax','special-defense','defense');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (18,'quirky',NULL,NULL);
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (19,'naive','special-defense','speed');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (20,'brave','speed','attack');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (21,'relaxed','speed','defense');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (22,'quiet','speed','special-attack');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (23,'sassy','speed','special-defense');
-INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) VALUES (24,'serious',NULL,NULL);
+INSERT INTO natures(FIELD1,nature_name,decreased_stat,increased_stat) 
+VALUES (0,'hardy',NULL,NULL),
+       (1,'bold','attack','defense'),
+       (2,'modest','attack','special-attack'),
+       (3,'calm','attack','special-defense'),
+       (4,'timid','attack','speed'),
+       (5,'lonely','defense','attack'),
+       (6,'docile',NULL,NULL),
+       (7,'mild','defense','special-attack'),
+       (8,'gentle','defense','special-defense'),
+       (9,'hasty','defense','speed'),
+       (10,'adamant','special-attack','attack'),
+       (11,'impish','special-attack','defense'),
+       (12,'bashful',NULL,NULL),
+       (13,'careful','special-attack','special-defense'),
+       (14,'rash','special-defense','special-attack'),
+       (15,'jolly','special-attack','speed'),
+       (16,'naughty','special-defense','attack'),
+       (17,'lax','special-defense','defense'),
+       (18,'quirky',NULL,NULL),
+       (19,'naive','special-defense','speed'),
+       (20,'brave','speed','attack'),
+       (21,'relaxed','speed','defense'),
+       (22,'quiet','speed','special-attack'),
+       (23,'sassy','speed','special-defense'),
+       (24,'serious',NULL,NULL);
